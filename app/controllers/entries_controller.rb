89630259e6c28e7f91c60e277659a77524599520
Katarzyna Mildner <itsyehudit@gmail.com>
2018-07-24 11:59:57 +0200

    NF: adding and saving entries

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,4 +1,16 @@
 class EntriesController < ApplicationController
   def new
   end
+
+  def create
+    @entry = Entry.new(entry_params)
+
+    @entry.save
+    redirect_to @entry
+  end
+
+  private
+    def entry_params
+      params.require(:entry).permit(:title, :content)
+    end
 end
