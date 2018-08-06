5b97bcdcb98a40c2a75a469a0e12e7522a890575
Katarzyna Mildner <itsyehudit@gmail.com>
2018-08-06 10:41:42 +0200

    Test: entry - content length validation

--- spec/models/entry_spec.rb
+++ spec/models/entry_spec.rb
@@ -17,5 +17,22 @@ RSpec.describe Entry do
       result2 = entry2.valid?
       expect(result2).to eq true
     end
+
+    it 'validates content minumim length' do
+      entry = Entry.new
+      entry.title = 'abc'
+      entry.content = '123'
+      result = entry.valid?
+      expect(result).to eq false
+      error = entry.errors.any?
+      expect(error).to eq true
+      msgs = entry.errors.full_messages.first
+      expect(msgs).to eq 'Content is too short (minimum is 5 characters)'
+      entry2 = Entry.new
+      entry2.title = 'abc'
+      entry2.content = '12345'
+      result2 = entry2.valid?
+      expect(result2).to eq true
+    end
   end
 end
