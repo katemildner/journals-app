711432aa375d83688a19199936f1c0b6cd794d4e
Katarzyna Mildner <itsyehudit@gmail.com>
2018-08-07 14:10:24 +0200

    Test refactor: entry title validation test done with Shoulda Matchers

--- spec/models/entry_spec.rb
+++ spec/models/entry_spec.rb
@@ -1,23 +1,11 @@
 require 'rails_helper'
 
+RSpec.describe Entry, type: :model do
+  it { is_expected.to validate_length_of(:title).is_at_least(3).with_message('is too short (minimum is 3 characters)') }
+end
+
 RSpec.describe Entry do
   describe 'validation' do
-    it 'validates title minimum length' do
-      entry = Entry.new
-      entry.title = 'ab'
-      result = entry.valid?
-      expect(result).to eq false
-      error = entry.errors.any?
-      expect(error).to eq true
-      msgs = entry.errors.full_messages.first
-      expect(msgs).to eq 'Title is too short (minimum is 3 characters)'
-      entry2 = Entry.new
-      entry2.title = 'abc'
-      entry2.content = '12345'
-      result2 = entry2.valid?
-      expect(result2).to eq true
-    end
-
     it 'validates content minumim length' do
       entry = Entry.new
       entry.title = 'abc'
